702aec5137b70417ca94055407a2671a4e60230d
Marco Aceti <mail@marcoaceti.it>
2021-07-26 15:13:16 +0200

    Better 'telegram' admin interfaces

--- telegram/admin.py
+++ telegram/admin.py
@@ -2,9 +2,43 @@ from django.contrib import admin
 from telegram.models import (
     User,
     Group,
+    GroupMembership,
 )
 
 
-@admin.register(User, Group)
-class BaseAdmin(admin.ModelAdmin):
-    pass
+class GroupMembershipInline(admin.TabularInline):
+    model = GroupMembership
+    extra = 1
+
+
+class GroupOwnerFilter(admin.SimpleListFilter):
+    title = "owner"
+    parameter_name = "owner"
+
+    def lookups(self, request, model_admin):
+        groups = Group.objects.all()
+        return (
+            (g.pk, g.owner) for g in groups
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() is None:
+            return queryset
+        return queryset.filter(owner=self.value())
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "reputation", "warn_count", "banned", "permissions_level", )
+    search_fields = ("id", "first_name", "last_name", )
+    fields = ("id", "first_name", "last_name", "reputation", "warn_count", "banned", "permissions_level", "last_seen", )
+    inlines = (GroupMembershipInline, )
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "owner", )
+    list_filter = (GroupOwnerFilter, )
+    search_fields = ("title", )
+    fields = ("id", "title", "description", "profile_picture", "invite_link", "owner", )
+    inlines = (GroupMembershipInline, )
