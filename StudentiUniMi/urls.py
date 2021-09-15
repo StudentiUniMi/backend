2016bd2b7337084670bd012502e92ef26bcc7209
Marco Aceti <mail@marcoaceti.it>
2021-09-15 23:52:12 +0200

    Add robots.txt and redirect to admin page instead of GitHub

--- StudentiUniMi/urls.py
+++ StudentiUniMi/urls.py
@@ -3,7 +3,9 @@
 The `urlpatterns` list routes URLs to views.
 """
 from django.conf import settings
+from django.conf.urls import url
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 from django.views.generic import RedirectView
 
@@ -14,10 +16,14 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(university.urls)),
     path('telegrambot/', include(telegrambot.urls)),
+    url(r'^robots.txt$', lambda r: HttpResponse(
+        "User-Agent: *\nDisallow: /",
+        content_type="text/plain",
+    ), name="robots_txt"),
 ]
 
 if not settings.DEBUG:
     urlpatterns.append(
-        path('', RedirectView.as_view(url="https://github.com/StudentiUniMi/backend"),
-             name='github-redirect'),
+        path('', RedirectView.as_view(url="https://api.studentiunimi.it/admin/"),
+             name='root-redirect'),
     )
