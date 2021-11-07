d6bbd47797cd4787c0f529c7f578e6c6bc0549e5
Mroik/PositiveC <mroik@delayed.space>
2021-11-07 22:38:03 +0100

    "Fixed" concurrency problem with /respects (#15)

--- telegrambot/handlers/memes.py
+++ telegrambot/handlers/memes.py
@@ -1,5 +1,5 @@
-from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
-from telegram.ext import CallbackContext
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, TelegramError
+from telegram.ext import CallbackContext, DispatcherHandlerStop
 
 
 def init_respects(update: Update, context: CallbackContext) -> None:
@@ -25,14 +25,23 @@ def add_respect(update: Update, context: CallbackContext) -> None:
     count = int(text.split("\n")[1].split(" ")[0])
     count += 1
 
-    message.edit_text(
-        f"Press F to pay respects.\n{count} users have paid their respects",
-        reply_markup=InlineKeyboardMarkup([
-            [
-                InlineKeyboardButton(
-                    text="🇫",
-                    callback_data="press_f",
-                )
-            ]
-        ])
-    )
+    try:
+        message.edit_text(
+            f"Press F to pay respects.\n{count} users have paid their respects",
+            reply_markup=InlineKeyboardMarkup([
+                [
+                    InlineKeyboardButton(
+                        text="🇫",
+                        callback_data="press_f",
+                    )
+                ]
+            ])
+        )
+    except TelegramError:
+        # Should probably resend the request with the updated counter
+        # but doing so would be a pain, 'cause the library checks update_ids
+        # so making fake requests would be too much effort for a meme command.
+        # Therefore we just ignore the requests that raise exceptions. The users
+        # will think that they're the only ones that clicked the button when in
+        # reality, more could have pressed it at the same time.
+        raise DispatcherHandlerStop
